daff6e308b68b6d76715b7cd68278f74550fd52a
crazybrain23 <44417085+crazybrain23@users.noreply.github.com>
2022-12-15 12:01:59 +0000

    Improvements and Fixes
    Added a conditional to adapt to the index.js not being in the same directory. Also changed `git fetch` to `git pull` as I think I used the wrong command before.

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 # exit if there's an arror
 set -e
-cd "$(dirname "$0")"
+# sets the directory variable to the scripts dir
+script_dir="$(dirname "$0")"
+if test -f $script_dir/index.js; then
+    # if the bot is in the same dir
+    bot_dir="$script_dir"
+elif test -f $script_dir/../index.js; then
+    # if the bot is in the parent dir
+    bot_dir="$script_dir/../"
+else
+    # if index.js s not found then exit
+    echo 'Cannot find the bot directory'
+    exit
+fi
+
+cd $bot_dir
 echo 'Fetching updates from GitHub'
 git fetch https://github.com/WillShaw1100/BiscuitBot
 echo 'Installing node packages'
 npm install
-
