4c9bcb2c03b821f042d88661f1f3f178a473665a
WillShaw1100 <williamrockstar110@gmail.com>
2024-05-24 15:10:36 +0100

    Presence, Mongo 7, old dependancy

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,19 +1,18 @@
 const Discord = require("discord.js")
-const { owners } = require("../index.js")
 const xpSchema = require("../models/xpSchema");
 
 module.exports = {
     name: "messageCreate",
-    run: async function runAll(bot, message){
-        const {client, prefix, owners} = bot
-        if(!message.guild) return
+    run: async function runAll(bot, message) {
+        const { client, prefix, owners } = bot
+        if (!message.guild) return
 
-        if(message.author.bot) return
+        if (message.author.bot) return
         let member = message.member
         let guildId = message.guild.id;
 
-        try{
-            xpSchema.findOne({ Guild: guildId}, async (err, data) => {
+        try {
+            xpSchema.findOne({ Guild: guildId }, async (err, data) => {
                 if (!data) {
                     await xpSchema.create({
                         Guild: guildId,
@@ -21,38 +20,38 @@ module.exports = {
                         Messages: 1,
                         XP: 0
                     });
-                }else if (data) {
-                    await xpSchema.updateOne({Messages: data.Messages + 1 });
-                    }
+                } else if (data) {
+                    await xpSchema.updateOne({ Messages: data.Messages + 1 });
+                }
             })
-        }catch(err){
+        } catch (err) {
             console.log(err);
             message.reply("Something went wrong. Please contact the developer.")
         }
-        
-        
-        if(!message.content.startsWith(prefix)) return
 
-        
+
+        if (!message.content.startsWith(prefix)) return
+
+
         const args = message.content.slice(prefix.length).trim().split(/ +/g)
         const cmdstr = args.shift().toLowerCase()
 
         let command = client.commands.get(cmdstr)
-        if(!command) return
+        if (!command) return
 
-        
 
-        if (command.devOnly && !owners.includes(member.id)){
+
+        if (command.devOnly && !owners.includes(member.id)) {
             return message.reply("This command is only available to the Bot Developer")
         }
-        if (command.permissions && member.permissions.missing(command.permissions).length !== 0){
-            return message.reply({content: "You do not have permission for this command", ephemeral: true})
+        if (command.permissions && member.permissions.missing(command.permissions).length !== 0) {
+            return message.reply({ content: "You do not have permission for this command", ephemeral: true })
         }
 
         try {
-            await command.run({...bot, message, args})
+            await command.run({ ...bot, message, args })
         }
-        catch(err){
+        catch (err) {
             let errMsg = err.toString()
 
             if (errMsg.startsWith("?")) {
--- slashcommands/version.js
+++ slashcommands/version.js
@@ -3,29 +3,29 @@ const { Channel, Message, EmbedBuilder } = require("discord.js")
 
 
 const run = async (client, interaction) => {
-    const bot_version = "2.7";
+    const bot_version = "2.7a";
     const bot_owner = "williamshaw";
     const server_count = client.guilds.cache.size;
 
-    try{
+    try {
         const botInfoEmbed = new EmbedBuilder()
-        .setTitle("Bot Info")
-        .setDescription("Information about the Bot.")
-        .setColor('Random')
-        .addFields([
-            {name: 'Bot Version:', value: `${bot_version}` },
-            {name: 'Lead Developer:', value: `${bot_owner}` },
-            {name: 'Server Count', value: `${server_count}` },
-        ])
+            .setTitle("Bot Info")
+            .setDescription("Information about the Bot.")
+            .setColor('Random')
+            .addFields([
+                { name: 'Bot Version:', value: `${bot_version}` },
+                { name: 'Lead Developer:', value: `${bot_owner}` },
+                { name: 'Server Count', value: `${server_count}` },
+            ])
 
-        interaction.reply( { embeds: [botInfoEmbed] })
+        interaction.reply({ embeds: [botInfoEmbed] })
 
-    }catch(err){
-    if (err){
-        console.error(err)
-        return interaction.reply('Failed to perform this command')
+    } catch (err) {
+        if (err) {
+            console.error(err)
+            return interaction.reply('Failed to perform this command')
+        }
     }
-}
 }
 module.exports = {
     name: "version",
