33f76e404db0d9087bdb7fabdbd15d0a5c71a639
WillShaw1100 <williamrockstar110@gmail.com>
2024-05-31 15:38:46 +0100

    Fix: Version from last commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     owners: ["206068051295076352", "475462382089535498"], // me, marcus
     prefix: process.env.PREFIX,
-    version: "2.9preview",
+    version: "2.91preview",
     bot_owner: "williamshaw"
 };
